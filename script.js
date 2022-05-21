df37fdacd16db73134b931cde10d7d4d023c6cc2
Katarzyna Grześkowiak <tabita33@gmail.com>
2022-05-22 00:25:38 +0200

    feat: adding separate index and form, adding button

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let header = document.createElement('header');
+header.classList.add('header');
+document.body.append(header)
+
+let h1 = document.createElement('h1');
+const textNode = document.createTextNode("Sorry for not creating JavaScript Bookshop page :(")
+h1.appendChild(textNode);
+document.body.appendChild(h1);
+
+let button = document.createElement('button');
+button.innerHTML = "Order Form";
+document.body.appendChild(button);
+
+button.addEventListener("click", myFunction);
+
+function myFunction() {
+    location.href="./form.html";
+}
+
+
 
