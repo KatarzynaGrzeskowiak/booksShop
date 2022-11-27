51552e6c4d5b805103c0875b938b4212f7f6e45b
Katarzyna Grześkowiak <tabita33@gmail.com>
2022-11-27 23:01:39 +0100

    feat: created containers for catalog and cart

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-let header = document.createElement('header');
+const container = document.createElement('div');
+container.classList.add('container');
+document.body.append(container);
+
+
+const header = document.createElement('header');
 header.classList.add('header');
-document.body.append(header)
+container.append(header);
 
-let h1 = document.createElement('h1');
-const textNode = document.createTextNode("Book Shop")
+const h1 = document.createElement('h1');
+const textNode = document.createTextNode("Book Shop");
 h1.appendChild(textNode);
-document.body.appendChild(h1);
+header.appendChild(h1);
 
-let button = document.createElement('button');
+const main = document.createElement('main');
+main.classList.add('main');
+container.appendChild(main);
+
+const catalogContainer = document.createElement('div');
+catalogContainer.classList.add('catalog-container');
+main.appendChild(catalogContainer);
+
+const cartContainer = document.createElement('div');
+cartContainer.classList.add('cart-container');
+main.appendChild(cartContainer);
+
+const catalog = new DocumentFragment();
+catalog.append(catalogContainer);
+
+fetch("./assets/JSON/books.json")
+    .then(response => response.json())
+    .then( data =>{
+        console.log(data);
+    })
+    .catch(error=>{
+        console.error(error);
+    });
+// OrderForm
+
+const button = document.createElement('button');
 button.innerHTML = "Order Form";
 document.body.appendChild(button);
 
@@ -16,14 +46,3 @@ button.addEventListener("click", myFunction);
 function myFunction() {
     location.href="./form.html";
 }
-
-
-
-
-const fragment = document.createDocumentFragment();
-
-const app = new BookShop(frag);
-document.body.append(frag);
-fetch('./books.json').then(res=>res.json()).then(booksData=>{
-  app.update(booksData);
-})
